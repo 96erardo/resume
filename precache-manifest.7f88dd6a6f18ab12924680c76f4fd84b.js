85f402f302f6a3a1f889cd9726d5e1d1c036e6e5
Gerardo García <gerardoagl96@gmail.com>
2019-03-03 11:56:59 -0400

    Updates

---
+++ precache-manifest.7f88dd6a6f18ab12924680c76f4fd84b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c834b4058e2737bb9f88a07ccddc8bc",
+    "url": "/resume/static/media/Rubik-Medium.1c834b40.ttf"
+  },
+  {
+    "revision": "86e0debdbe30b93fc66e",
+    "url": "/resume/static/css/main.9d3d341a.chunk.css"
+  },
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.fd78f13f.js"
+  },
+  {
+    "revision": "da409f470755a9cd7185",
+    "url": "/resume/static/js/2.da409f47.chunk.js"
+  },
+  {
+    "revision": "3e97dbdbf4aa4665df1da7bc816d93ac",
+    "url": "/resume/static/media/Oswald-Medium.3e97dbdb.ttf"
+  },
+  {
+    "revision": "7b62782759df1aeb75ba8b3a71d72871",
+    "url": "/resume/static/media/Rubik-Light.7b627827.ttf"
+  },
+  {
+    "revision": "dfd89c5788f6eb95e952b41f40c342f6",
+    "url": "/resume/static/media/Rubik-LightItalic.dfd89c57.ttf"
+  },
+  {
+    "revision": "db5c7abfb1b57df0ae8ff1c236cbd810",
+    "url": "/resume/static/media/Rubik-Regular.db5c7abf.ttf"
+  },
+  {
+    "revision": "5c0f34ddd45e3cf75ce29a3b9595ded8",
+    "url": "/resume/static/media/Rubik-Italic.5c0f34dd.ttf"
+  },
+  {
+    "revision": "d43efa6b8630dd2a1dcc1b35e8f9db41",
+    "url": "/resume/static/media/Rubik-Bold.d43efa6b.ttf"
+  },
+  {
+    "revision": "86e0debdbe30b93fc66e",
+    "url": "/resume/static/js/main.86e0debd.chunk.js"
+  },
+  {
+    "revision": "8bd53923acb5be36dc4da06f07ecfaf8",
+    "url": "/resume/static/media/Rubik-MediumItalic.8bd53923.ttf"
+  },
+  {
+    "revision": "158dcc8a80cb7dfa4173288aecd77e92",
+    "url": "/resume/static/media/Rubik-BoldItalic.158dcc8a.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/resume/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/resume/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/resume/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "d1efcd4d126837fe0dcf9b6cf3a00d64",
+    "url": "/resume/static/media/Roboto-LightItalic.d1efcd4d.ttf"
+  },
+  {
+    "revision": "42bbe4eefcde1297b11dc4b6491e9746",
+    "url": "/resume/static/media/Roboto-Italic.42bbe4ee.ttf"
+  },
+  {
+    "revision": "1eb7a893589ddce89d81cdb22a356660",
+    "url": "/resume/static/media/Roboto-BoldItalic.1eb7a893.ttf"
+  },
+  {
+    "revision": "5c1a2c3b266df5bb9ad87035c4bd862d",
+    "url": "/resume/index.html"
+  }
+];
